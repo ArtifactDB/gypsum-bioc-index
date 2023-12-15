8d85e67cb86e07bd1b7e3e2656ea3da7bf3fb3e7
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-12-15 07:19:38 -0800

    Minor schema fix, added validator test code.

--- src/validatorFromSchema.js
+++ src/validatorFromSchema.js
@@ -2,5 +2,6 @@ import Ajv from "ajv";
 
 export function validatorFromSchema(schema) {
     const ajv = new Ajv();
+    ajv.addKeyword("_attributes")
     return ajv.compile(schema);
 }
---
+++ tests/validatorFromSchema.test.js
@@ -0,0 +1,14 @@
+import * as val from "../src/validatorFromSchema.js";
+import schema from "../schemas/bioconductor.json";
+import { examples } from "./examples.js";
+
+test("validatorFromSchema works as expected", async () => {
+    const validator = val.validatorFromSchema(schema);
+    expect(validator(examples[0])).toBe(true);
+    expect(validator(examples[1])).toBe(true);
+
+    let test = {...(examples[0])};
+    delete test.title;
+    expect(validator(test)).toBe(false);
+    expect(validator.errors[0].message).toMatch("title");
+})
