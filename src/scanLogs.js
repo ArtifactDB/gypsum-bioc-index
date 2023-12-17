94558175fb0851b9724876a3602f0ee8b87e3ad7
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-12-16 16:18:40 -0800

    Improved explanation for StartAfter= in scanLogs.

--- src/scanLogs.js
+++ src/scanLogs.js
@@ -3,13 +3,14 @@ import { quickList } from "./utils.js";
 export async function scanLogs(since) {
     const prefix = "..logs/";
 
-    // Rewinding a day and using that as the starting key.  The idea is to
-    // improve the efficiency of the list operation by skipping things that
-    // we must have already processed. Note, though, that this part is only
+    // Rewinding a day and using that as the 'StartAfter' key. The idea is to
+    // improve the efficiency of the list operation by skipping keys that we
+    // must have already processed. Note, though, that this part is only
     // approximate as an alphanumeric sort doesn't check for timezones or
-    // account for variable seconds. The rewinding by a day ensures that we
-    // don't skip things we might still need to process, but in any case,
-    // we still need to check the times manually to 'since'.
+    // account for variable precision of fractional seconds. Hence, we rewind
+    // by a full day to be conservative and ensure that we only skip things
+    // that we must have already processed; for everything else, we still list
+    // the keys, extract the times and compare them manually to 'since'.
     let after = new Date(since.getTime() - 24 * 60 * 60 * 1000);
     let after_name = prefix + String(after.getYear()) + "-" + String(after.getMonth()) + "-" + String(after.getDate())
 
