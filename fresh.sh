80dbe03a573d1d482fc86fe8ea019b3ab8652c92
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-02-20 14:22:20 -0800

    Updated for the renamed repo.

--- fresh.sh
+++ fresh.sh
@@ -3,14 +3,9 @@
 set -e
 set -u
 
-args=""
-for x in $(ls schemas)
-do
-    dbname=$(echo $x | sed "s/json/sqlite3/")
-    fname=$(echo "_${x}")
-    args="${args} --config ${fname},${dbname}"
-done
-
 rm -rf build
 mkdir build
-NODE_OPTIONS='--experimental-fetch' npx --package=stringy-sqlite-search fresh ${args} --gypsum https://gypsum.artifactdb.com --dir build
+NODE_OPTIONS='--experimental-fetch' npx --package=gypsum-metadata-index fresh \
+    --class _bioconductor.json,bioconductor.sqlite3 \
+    --gypsum https://gypsum.artifactdb.com \
+    --dir build
