51cbcc3d900dc3549f9a3a5e750363a58c0a56c6
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-02-20 10:48:50 -0800

    No need for schema-based configuration any more.

--- update.sh
+++ update.sh
@@ -4,9 +4,11 @@ set -e
 set -u
 
 args=""
-for x in $(ls .config)
+for x in $(ls schemas)
 do
-    args="${args} --config .config/${x}"
+    dbname=$(echo $x | sed "s/json/sqlite3/")
+    fname=$(echo "_${x}")
+    args="${args} --config ${fname},${dbname}"
 done
 
 NODE_OPTIONS='--experimental-fetch' npx --package=stringy-sqlite-search update ${args} --gypsum https://gypsum.artifactdb.com --dir build
